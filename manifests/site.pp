65a2ea62734f601bbb15a562c238daa19103ca95
clericeon <clericeon@gmail.com>
2021-04-14 13:59:10 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node default {
-  file { '/root/README':
-    ensure => file,
-    content => 'This is a readme',
-    owner   => 'root',
-  }
+}
+node 'server.puppet.ubuntu' {
+  include role::master_server
 }
